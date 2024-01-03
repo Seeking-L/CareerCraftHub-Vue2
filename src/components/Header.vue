b0909a088ac0e13b5a1384ea904eb3ea98f7635a
LJQ <82761283+Seeking-L@users.noreply.github.com>
2024-01-03 16:44:49 +0800

    123

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,6 +8,7 @@
         <i class="iconfontstats icon-stats"></i>
         <i class="iconfont icon-job"></i>
         <i class="iconfont icon-study"></i>
+        <authImg :imgUrl="avatarSrc" :authToken="tokenStr" alt="Image" @click.native="pushShow"></authImg>
       </div>
       <!-- <div class="navbar-right nav-links">
         <router-link active-class="active-link" to="/mainpage/stats">Stats</router-link>
@@ -20,9 +21,55 @@
 
 
 <script>
-
+import authImg from "./AuthImg.vue";
 export default {
   name: "IndexHeader",
+  components:{authImg,},
+  data() {
+    return {
+      userName: "",
+      userPhone: "",
+      userId: "",
+      password: "",
+      avatar: "",
+      
+    };
+  },
+  computed: {
+    avatarSrc: function () {
+      const prefix = "http://localhost:8080/";
+      if (!this.avatar) return prefix + "statics/headpicture.jpg";
+      return prefix + "Users/" + this.userId + "/" + this.avatar;
+    },
+    tokenStr:function(){
+      return localStorage.getItem("token");
+    }
+  },
+  methods: {
+    pushShow() {
+      this.$router.push({
+        path: "/login",
+      });
+    },
+    replaceShow() {
+      this.$router.replace({
+        path: "/login",
+      });
+    },
+  },
+  created() {
+    const tokenStr = localStorage.getItem("token");
+    // 如果没有获取到token或user，返回登录页
+    if (!tokenStr) this.$router.push("/login");
+    const userStr = localStorage.getItem("user");
+    if (!userStr) this.$router.push("/login");
+    const user = JSON.parse(userStr);
+    this.userName = user.userName;
+    this.userId = user.userId;
+    this.userPhone = user.userPhone;
+    this.password = user.password;
+    this.avatar = user.avatar;
+  },
 };
 </script>
 
@@ -66,7 +113,6 @@ header {
 .navbar-right i:last-child {
   margin-right: 100px; /* 调整最后一个图标的右侧间距 */
 }
-
 
 .navbar-left.nav-icons {
   display: flex;
@@ -146,5 +192,24 @@ header {
   width:90%;
   z-index: 100; /*可以调整z-index，确保导航栏在其他元素之上*/
   box-shadow: 0pt 0.052083333in 11.25pt -5.25pt rgba(0, 0, 0, 0.1);
+}
+
+/* 图片样式 */
+.rectangle-module img {
+  display: block;
+  margin: -200px auto 0; /* 将左右外边距设置为 auto，使图片在水平方向上居中 */
+
+  clip-path: circle(50% at center); /* 使用clip-path将图片截取为圆形 */
+
+  /*依赖长方形模块的水平、垂直长度来保持圆形 */
+  width: 3%;
+  height: 2%;
+
+  max-width: 10%; /* 设置图片最大宽度为100% */
+  border-radius: 5%; /* 将图片的边框半径设置为50%，使其呈圆形 */
+  transition: transform 0.3s ease-in-out;
+}
+.rectangle-module img:hover {
+  transform: scale(1.1);
 }
 </style>
